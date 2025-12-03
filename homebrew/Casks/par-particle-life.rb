188a011650b7fb25f43a0a8682741cc93f9b63d8
Paul Robello <probello@gmail.com>
2025-12-02 22:20:48 -0800

    chore: update cask description

--- homebrew/Casks/par-particle-life.rb
+++ homebrew/Casks/par-particle-life.rb
@@ -7,7 +7,7 @@ cask "par-particle-life" do
 
   url "https://github.com/paulrobello/par-particle-life/releases/download/v#{version}/par-particle-life-macos-#{arch}.zip"
   name "par-particle-life"
-  desc "GPU-accelerated particle life simulation in Rust"
+  desc "GPU-accelerated particle life simulation in Rust using wgpu"
   homepage "https://github.com/paulrobello/par-particle-life"
 
   depends_on macos: ">= :catalina"
